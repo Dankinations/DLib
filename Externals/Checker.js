a21fc7a6d62aae0e900f9080e6960180b0cdb561
Danki <129652478+Dankinations@users.noreply.github.com>
2025-03-29 11:25:16 +0200

    going back cuz wtf am i doing

--- Externals/Checker.js
+++ Externals/Checker.js
@@ -6,58 +6,36 @@
         sessionStorage.setItem(key, '1'); // Set instantly to prevent multiple alerts happening
         const message = "Ey so you don't have DLib enabled/downloaded, some userscripts use DLib and since you don't have DLib enabled, they  will break, please download/enable DLib :P";
 
-        BootstrapDialog.show({
-            title: 'Oh No!',
-            type: BootstrapDialog.TYPE_WARNING,
-            message,
-            buttons: [
-                {
-                    label: 'Download DLib',
-                    cssClass: 'btn-primary',
-                    action(dialog) {
-                        open("https://github.com/Dankinations/DLib/releases/latest/download/DLib.user.js");
+        if (window.SimpleToast) {
+            SimpleToast({
+                title: 'Missing Requirements',
+                text: message,
+                footer: pluginName,
+            });
+        } else if (window.BootstrapDialog) {
+            BootstrapDialog.show({
+                title: 'Oh No!',
+                type: BootstrapDialog.TYPE_WARNING,
+                message,
+                buttons: [
+                    {
+                        label: 'Download DLib',
+                        cssClass: 'btn-primary',
+                        action(dialog) {
+                            open("https://github.com/Dankinations/DLib/releases/latest/download/DLib.user.js");
+                        },
                     },
-                },
-                {
-                    label: 'Proceed Anyways',
-                    cssClass: 'btn-danger',
-                    action(dialog) {
-                        dialog.close();
-                    },
-                }],
-        });
-
-        // if (1 == 0) {
-        //     console.log("hi")
-        //     SimpleToast({
-        //         title: 'Missing Requirements',
-        //         text: message,
-        //         footer: pluginName,
-        //     });
-        // } else if (window.BootstrapDialog) {
-        //     BootstrapDialog.show({
-        //         title: 'Oh No!',
-        //         type: BootstrapDialog.TYPE_WARNING,
-        //         message,
-        //         buttons: [
-        //             {
-        //                 label: 'Download DLib',
-        //                 cssClass: 'btn-primary',
-        //                 action(dialog) {
-        //                     open("https://github.com/Dankinations/DLib/releases/latest/download/DLib.user.js");
-        //                 },
-        //             },
-        //             {
-        //                 label: 'Proceed Anyways',
-        //                 cssClass: 'btn-danger',
-        //                 action(dialog) {
-        //                     dialog.close();
-        //                 },
-        //             }],
-        //     });
-        // } else {
-        //     sessionStorage.removeItem(key)
-        // }
+                    {
+                        label: 'Proceed Anyways',
+                        cssClass: 'btn-danger',
+                        action(dialog) {
+                            dialog.close();
+                        },
+                    }],
+            });
+        } else {
+            sessionStorage.removeItem(key)
+        }
     }
 
     throw new Error(`${pluginName}: DLib required`);
